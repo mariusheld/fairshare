9ccb85d5c723daa9aa60f087509e4158628a2dbf
mariusheld <116557080+mariusheld@users.noreply.github.com>
2023-01-19 19:02:18 +0100

    Logo Größe angepasst

--- pages/011_foodsaver_anmeldung.php
+++ pages/011_foodsaver_anmeldung.php
@@ -79,7 +79,7 @@ if ($userunbekannt == "unbekannt") {
 
 <body>
     <header style="padding-left: 0;">
-        <a href="../index.php" ><img src="../media/logo.svg" style="margin-left:0"></a>
+        <a href="../index.php" ><img src="../media/logo.svg" style="margin-left:0" width="200px"></a>
         <div href="#" class="MitarbeiterLogin" id="login">
             <span>Mitarbeiter*in</span>
             <button id="startscreen-mitarbeiter-button"><svg id="login-logo" xmlns="http://www.w3.org/2000/svg"
