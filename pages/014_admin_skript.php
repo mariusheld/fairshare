5cf31d3fc90799667c810cd70ab896e00c0e6558
Danilo Ley Alves <daniloleyalves1@hotmail.com>
2023-01-22 01:16:53 +0100

    more fixes

--- pages/014_admin_skript.php
+++ pages/014_admin_skript.php
@@ -50,17 +50,19 @@ if (isset($_GET['mfwArrayFairteilen'])) {
   $myArrayObjects = explode("|", $myArrayString);
 
   foreach ($myArrayObjects as $key => $value) {
-    $myArray = explode(",", $value);
-    $lmkey = $myArray[0];
-    $bewegMenge = $myArray[1];
-    $LStatusKey = 2;
-    $EntsorgenQuery = $db->prepare("INSERT INTO `Bestand_Bewegung` (`LMkey`, `LStatusKey`, `BewegDatum`, `BewegMenge`, `EntsorgGrund`)
+    if ($value != "") {
+      $myArray = explode(",", $value);
+      $lmkey = $myArray[0];
+      $bewegMenge = $myArray[1];
+      $LStatusKey = 2;
+      $EntsorgenQuery = $db->prepare("INSERT INTO `Bestand_Bewegung` (`LMkey`, `LStatusKey`, `BewegDatum`, `BewegMenge`, `EntsorgGrund`)
         VALUES ('$lmkey', '$LStatusKey', now(), '$bewegMenge', NULL)");
-    $erfolg = $EntsorgenQuery->execute();
-    // Fehlertest
-    if (!$erfolg) {
-      $fehler = $query->errorInfo();
-      die("Folgender Datenbankfehler ist aufgetreten:" . $fehler[2]);
+      $erfolg = $EntsorgenQuery->execute();
+      // Fehlertest
+      if (!$erfolg) {
+        $fehler = $query->errorInfo();
+        die("Folgender Datenbankfehler ist aufgetreten:" . $fehler[2]);
+      }
     }
   }
   header("Location: admin.php?success=true");
